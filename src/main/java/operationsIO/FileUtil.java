86bffb47b06574d8d3a63438d4fd4dd0b5b1ffad
Tomasz Wojtyra <t.wojtyra@il-pib.pl>
2023-01-19 20:50:27 +0100

    reading from file

--- src/main/java/operationsIO/FileUtil.java
+++ src/main/java/operationsIO/FileUtil.java
@@ -4,21 +4,28 @@ import java.io.*;
 
 public class FileUtil {
 
-    public String readFile(String path) throws IOException {
-        var fileReader = new FileReader(path);
-        var bufferedReader = new BufferedReader(fileReader);
+    public String readFile(String path) {
         var stringBuilder = new StringBuilder();
-        String line;
-        while ((line = bufferedReader.readLine()) != null) {
-            stringBuilder.append(line);
+        try(BufferedReader bf = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = bf.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
         return stringBuilder.toString();
     }
 
-    public void writeFile(String path, String content) throws IOException {
-        var fileWriter = new FileWriter(path);
-        var bufferedWriter = new BufferedWriter(fileWriter);
-        bufferedWriter.write(content);
-        bufferedWriter.flush();
+    public void writeFile(String path, String content) {
+        BufferedWriter bufferedWriter;
+        try {
+            bufferedWriter = new BufferedWriter(new FileWriter(path));
+            bufferedWriter.write(content);
+            bufferedWriter.flush();
+            bufferedWriter.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
