59ae17767805453e31d073b04b226134b9d38c13
Tomasz Wojtyra <t.wojtyra@il-pib.pl>
2023-01-19 16:44:54 +0100

    streams

--- src/main/java/collections/streams/Exercises.java
+++ src/main/java/collections/streams/Exercises.java
@@ -3,8 +3,6 @@ package collections.streams;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.BiConsumer;
-import java.util.stream.Collectors;
 
 class Exercises {
 
