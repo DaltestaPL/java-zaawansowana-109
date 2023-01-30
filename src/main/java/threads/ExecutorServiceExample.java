614e0c8227c4c61d7dc8d6c2e6c8375df6065306
Tomasz Wojtyra <t.wojtyra@il-pib.pl>
2023-01-30 20:17:56 +0100

    threads

--- src/main/java/threads/ExecutorServiceExample.java
+++ src/main/java/threads/ExecutorServiceExample.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicReference;
-import java.util.stream.Collectors;
 
 public class ExecutorServiceExample {
 
